db12ac30842c9235330707b92c2f35b82e6dcc2c
whatdoineed2do <whatdoineed2do@ .com>
2017-09-09 13:28:58 +0100

    initial flexbox output

--- ImgHtml.cc
+++ ImgHtml.cc
@@ -17,19 +17,31 @@ ImgHtml*  ImgHtml::create(const char* type_)
     };
 
     _HtmlGenerators  htmlgens[] = {
-        { ImgHtmlClassic::ID, []() { return (ImgHtml*)new ImgHtmlClassic(); } },
         { ImgHtmlFlexbox::ID, []() { return (ImgHtml*)new ImgHtmlFlexbox(); } },
+        { ImgHtmlClassic::ID, []() { return (ImgHtml*)new ImgHtmlClassic(); } },
         { nullptr, nullptr }
     };
 
+    const bool options = type_ && strcasecmp("help", type_) == 0; 
+    if (options) {
+        cout << "html options[]=";
+    }
+
     ImgHtml*  h = NULL;
     _HtmlGenerators*  p = htmlgens;
-    while (h == NULL && p->id) {
-        if (type_ == NULL || strcasecmp(p->id, type_) == 0) {
-            h = p->create();
+    while (h == NULL && p->id) 
+    {
+        if (options) {
+            cout << p->id << " ";
+        }
+        else {
+            if (type_ == NULL || strcasecmp(p->id, type_) == 0) {
+                h = p->create();
+            }
         }
         ++p;
     }
+    if (options) cout << endl;
     return h;
 }
 
@@ -86,7 +98,7 @@ std::string  ImgHtmlClassic::generate(ImgHtml::Payloads& payloads_)
             if (!img.rating.empty()) {
                 html << "[" << img.rating << "] ";
             }
-            html << img.title << " (" << t->idx().key.dt.hms << ") Exif: " << img << "\">"
+            html << img.title << " (" << t->idx().key.dt.hms << ")\nExif: " << img << "\">"
                  << "<a href=\"" << img.filename << "\"><img src=\"" << t->prevpath() << "\"></a>";
 
             ImgIdx::Imgs::const_iterator  alts = t->idx().imgs.begin();
@@ -117,7 +129,130 @@ std::string  ImgHtmlClassic::generate(ImgHtml::Payloads& payloads_)
 
 std::string  ImgHtmlFlexbox::generate(ImgHtml::Payloads& payloads_)
 {
+    static const char* const  hdr = "\
+<html lang=en><meta charset=utf-8><head>\
+<style>\
+body { margin: 0; background: #333; }\
+.flex-container {\
+  padding: .vw;\
+  font-size: 0;\
+  display: -ms-flexbox;\
+  -ms-flex-wrap: wrap;\
+  -ms-flex-direction: column;\
+  -webkit-flex-flow: row wrap;\
+  flex-flow: row wrap;\
+  display: -webkit-box;\
+  display: flex;\
+  margin: 0.5vw;\
+}\
+.flex-item {\
+  -webkit-box-flex: auto;\
+  -ms-flex: auto;\
+  flex: 200px;\
+  margin: 0.2vw;\
+}\
+.bottom-left { \
+  position: relative;\
+  bottom: 15px;\
+  left: 10px;\
+  font-size: 10px;\
+}\
+h2 {\
+  padding: .vw;\
+  padding-right: 5px;\
+  background: #333;\
+  text-align: right;\
+  color: white;\
+  margin: 0;\
+}\
+p {\
+  background: #333;\
+  padding-right: 10px;\
+  text-align: right;\
+  color: white;\
+  margin: 0;\
+}\
+img {\
+  width: 100%;\
+  height: auto;\
+  border-radius: 3px;\
+}\
+a {\
+  color: white;\
+}\
+a:hover {\
+  color: #333;\
+  background-color: white;\
+}\
+@media screen and (max-width: 400px) {\
+  header div { margin: 0; }\
+  header { padding: 0; }\
+\
+}\
+</style>\
+</head>\
+<body>\n";
+
     std::ostringstream  html;
-    //return std::move(html.str());
-    return html.str();
+    html << hdr;
+
+    for (auto& p : payloads_)
+    {
+        ImgIdx&        idx    = p.idx;
+        ImgThumbGens&  thumbs = p.thumbs;
+        
+        cout << "  working on [" << setw(3) << idx.size() << "]  " << idx.id << "  " << flush;
+        html << "<h2>/<a href=\"" << idx.id << "\">" << idx.id << "</a></h2>";
+
+        if (idx.empty()) {
+            cout << '\n';
+            continue;
+        }
+
+        idx.sort();
+
+
+        ImgIdx::const_iterator  dts = idx.begin();
+
+        html << "<p>" << dts->key.dt.hms;
+        advance(dts, idx.size()-1);
+        if (dts != idx.end()) {
+            html << " .. " << dts->key.dt.hms;
+        }
+        html << "</p>\n"
+             << "<div class=flex-container>\n";
+        
+        for (auto& t : thumbs)
+        {
+            const ImgData&  img = t->img();
+
+            // insert the caption for the cell
+            html << " <div class=flex-item  title=\"";
+            if (!img.rating.empty()) {
+                html << "[" << img.rating << "] ";
+            }
+            html << img.title << "  (" << t->idx().key.dt.hms << ")\n"
+                 << "Exif: " << img << "\">\n"
+                 << "  <a href=\"" << img.filename << "\"><img src=\"" << t->prevpath() << "\"></a>";
+
+            if (t->idx().imgs.size() > 1)
+            {
+                html << "   <div class=bottom-left>";
+                ImgIdx::Imgs::const_iterator  alts = t->idx().imgs.begin();
+                while (alts != t->idx().imgs.end()) {
+                    const ImgData&  altimg = *alts++;
+                    html << "<a href=\"" << altimg.filename << "\"># </a>";
+                }
+                html << "\n  </div>";
+            }
+            html << "</div>";
+                
+            cout << '.' << flush;
+        }
+        html << "</div>\n\n";
+        cout << endl;
+    }
+
+    html << "</body></html>";
+    return std::move(html.str());
 }
