ab21378658cfe254bb71eaf8f7fa85b19ac033f3
whatdoineed2do <whatdoineed2do@no.where.com>
2017-09-19 20:52:44 +0100

    move useful tmp vars to class scope

--- ImgAVFmtParser.h
+++ ImgAVFmtParser.h
@@ -2,6 +2,8 @@
 #define IMG_AVFMT_PARSER_H
 
 #include "ImgMetaParser.h"
+#include <string>
+
 
 class ImgAVFmtParser : public ImgMetaParser
 {
@@ -11,6 +13,9 @@ class ImgAVFmtParser : public ImgMetaParser
   private:
     const Img  _parse(const char* filename_, const struct stat&, const char* thumbpath_)  const
 	 throw (std::invalid_argument, std::range_error, std::underflow_error, std::overflow_error) override final;
+
+    mutable char  _averr[256];
+    mutable std::string  _tmp;
 
     static bool  _initd;
 };
