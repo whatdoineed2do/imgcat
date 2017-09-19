094c5e66f150c17c7927ae0dc5f86e6cf2bf2302
whatdoineed2do <whatdoineed2do@no.where.com>
2017-09-19 20:53:33 +0100

    use ffmpeg libavformat to pull basic meta

--- ImgAVFmtParser.cc
+++ ImgAVFmtParser.cc
@@ -27,8 +27,86 @@ const Img  ImgAVFmtParser::_parse(const char* filename_, const struct stat& st_,
 {
     ImgData  imgdata(filename_, st_.st_size);
     imgdata.type = ImgData::VIDEO;
-    ostringstream  tmp;
-    tmp << thumbpath_ << "/" << st_.st_dev << "-" << st_.st_ino;
-    imgdata.thumb = tmp.str();
+
+    {
+	ostringstream  tmp;
+	tmp << thumbpath_ << "/" << st_.st_dev << "-" << st_.st_ino;
+	imgdata.thumb = std::move(tmp.str());
+    }
+
+#if 1
+    AVFormatContext*  avfmt   = NULL;
+    AVCodecContext*   avcodec = NULL;
+
+    int ret;
+    if ((ret = avformat_open_input(&avfmt, filename_, NULL, NULL)) != 0) {
+	av_strerror(ret, _averr, sizeof(_averr));
+
+	std::ostringstream  err;
+	err << "failed avformat_open_input - " << filename_ << " - " << _averr;
+	throw std::invalid_argument(err.str());
+    }
+
+#if 0
+    while ((tag = av_dict_get(avfmt->metadata, "", tag, AV_DICT_IGNORE_SUFFIX))) {
+	std::cout << "  " << tag->key << "=" << tag->value << std::endl;
+    }
+#endif
+
+    imgdata.metavid.duration = avfmt->duration/AV_TIME_BASE;
+
+    avformat_find_stream_info(avfmt, NULL);
+    int  vidstream = -1;
+    for (int i=0; i< avfmt->nb_streams; i++) {
+	if (avfmt->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
+	    vidstream = i;
+	    break;
+	}
+    }
+    if (vidstream < 0) {
+	avformat_close_input(&avfmt);
+	std::ostringstream  err;
+	err << "failed avformat_open_input - " << filename_ << " - " << _averr;
+	throw std::underflow_error(err.str());
+    }
+
+    avcodec = avfmt->streams[vidstream]->codec;
+    {
+	std::ostringstream  dim;
+	dim << avcodec->width << "x" << avcodec->height;
+	imgdata.xy = std::move(dim.str());
+    }
+
+    imgdata.metavid.framerate = av_q2d(avcodec->framerate);
+
+    struct TagsOI {
+	//TagsOI(const char* tag_, const std::string& s_) : tag(tag_), s(s_) { }
+
+        const char*   tag;
+	std::string&  s;
+    }
+    toi[] = {
+	{ "major_brand",   imgdata.metavid.container },
+	{ "model",         imgdata.metavid.model },
+	{ "date",          imgdata.moddate },
+	{ "creation_time", imgdata.moddate },
+	{ NULL, _tmp }
+    };
+
+    const TagsOI*  pt = toi;
+    while (pt->tag)
+    {
+	AVDictionaryEntry*  avtag = NULL;
+	if ( (avtag = av_dict_get(avfmt->metadata, pt->tag, avtag, AV_DICT_IGNORE_SUFFIX)) ) {
+	    if (avtag->value && pt->s.empty()) {
+		pt->s = avtag->value;
+	    }
+	}
+
+	++pt;
+    }
+
+    avformat_close_input(&avfmt);
+#endif
     return Img(ImgKey(st_.st_ino, st_.st_mtime), imgdata);
 }
