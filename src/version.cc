0df24550b66d7028bce7ba453c9411a902b4d731
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-04-02 17:52:57 +0100

    generate VERSION based on git tag/hash

--- src/version.cc
+++ src/version.cc
@@ -1,18 +1,12 @@
 #include "version.h"
+#include <config.h>
 
 namespace Imgcat
 {
 
 const char*  version()
 {
-#ifdef IMGCAT_VERSION
-#define IMGCAT_STRM(x)  #x
-#define IMGCAT_STR(x)   IMGCAT_STRM(x)
-#define IMGCAT_VERSION_STR  IMGCAT_STR(IMGCAT_VERSION)
-#else
-#define IMGCAT_VERSION_STR  "v0.0.0-unknown"
-#endif
-    return IMGCAT_VERSION_STR;
+    return VERSION;
 }
 
 }
